69ee7bc69a63f51ac0c48430742f5c28ec70151c
arjun31 <arjunbanerjee13@gmail.com>
2025-02-12 13:29:51 +0530

    adding files

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div className='text-3xl flex flex-col justify-center items-center h-screen'>Vite+React+Tailwindcss</div>
+  )
+}
